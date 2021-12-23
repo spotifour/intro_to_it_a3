65cab46ee559bb47f53874a6f004aa232810ca98
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2021-12-23 19:56:49 +0700

    add projectDescription page

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './dropdown.css';
+import React, { useState } from "react";
+import "./dropdown.css";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 const Dropdown = (props) => {
-    return (
-        <div>
-
+  const [openDropdown, setOpenDropdown] = useState(false);
+  return (
+    <div className="dropdown">
+      <div className="dropdownHead" onClick={() => setOpenDropdown(!openDropdown)}>
+        <h4>{props.title}</h4>
+        <span>{openDropdown ? <AiOutlineMinus /> : <AiOutlinePlus />}</span>
+      </div>
+      {openDropdown ? (
+        <div className="dropdownBody">
+          {props.content}
         </div>
-    )
-}
+      ) : null}
+    </div>
+  );
+};
 
 export default Dropdown;
--- src/pages/ProjectDescription/ProjectDescription.js
+++ src/pages/ProjectDescription/ProjectDescription.js
@@ -1,8 +1,46 @@
 import React from "react";
 import "./ProjectDescription.css";
 
+import Dropdown from "../../components/Dropdown/Dropdown";
+import {
+  projectOverview,
+  projectDetail,
+} from "../../constants/projectDescription";
+
+const convertText = (data) => {
+  const detailSubSections = Object.keys(data).map((subsection) => {
+    const title = subsection.replaceAll("_", " ");
+    const content = data[subsection].map((para) => {
+      if (Array.isArray(para)) {
+        return (
+          <ul>
+            {para.map((item) => {
+              return <li>{item}</li>;
+            })}
+          </ul>
+        );
+      }
+      return <p>{para}</p>;
+    });
+    return <Dropdown title={title} content={content} />;
+  });
+  return detailSubSections;
+};
+
 const ProjectDescription = (props) => {
-  return <div>ProjectDescription</div>;
+
+  const overviewSubsections = convertText(projectOverview);
+  const detailSubSections = convertText(projectDetail);
+
+  return (
+    <div>
+      <h1 className="sectionTitle">Project Description</h1>
+      <h2 className="sectionTitle">Overview</h2>
+      <div className="subsection">{overviewSubsections}</div>
+      <h2 className="sectionTitle">Detailed Description</h2>
+      <div className="subsection">{detailSubSections}</div>
+    </div>
+  );
 };
 
 export default ProjectDescription;
